978ec01410713770c4a134a956ee6d0ff2450211
haoly <haoly@10.16.5.236>
2024-02-23 17:07:22 -0500

    commnent about the favorite subject

---
+++ src/Hao_Design.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author haoly
+ */
+public class Hao_Design {
+    //my favorite subject is PE( Physical Education)
+    
+}
